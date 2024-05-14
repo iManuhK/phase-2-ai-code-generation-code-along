0cfa6c898d241414f0b2fdbc8f6f7e4d965e3353
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-05-14 21:31:51 +0300

    AI code along

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+/**A book object representation.
+ * @typedef {Object} Book
+ * @property {String} title - The title of the book.
+ * @property {String} author - The author of the book.
+ * @property {String} genre - The genre of the book.
+ */
+
+function filterByGenre(genre, books) {
+  return books.filter((book) => book.genre === genre);
+}
+
+module.exports = filterByGenre;
+/**returns a list of books with the given genre.
+ * @param {String} genre - The genre of books required.
+ * @param {Book[]} books - An array of Book objects.
+ * @returns {Book[]} An Array of Book objects matching the specified genre.
+ */
